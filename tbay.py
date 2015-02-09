0eaa8a565cbe7b5cbf2d7165d8406731d9d33e55
John Moore <john@ncub8.com>
2015-02-09 15:24:31 +0000

    code s layed out but there's a run-time error

--- tbay.py
+++ tbay.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, DateTime
 
 
 engine = create_engine('postgresql://action:action@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
+
 
 class Item(Base):
   __tablename__ = "items"
@@ -18,19 +19,28 @@ class Item(Base):
   description = Column(String)
   start_time = Column(DateTime, default=datetime.utcnow)
   
+  user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
+  bids = relationship("Bid", backref="item")
+  
+
+  
+  
 class User(Base):
   __tablename__ = "users"
   
   id = Column(Integer, primary_key=True)
   username = Column(String, nullable=False)
   password = Column(String, nullable=False)
+  items = relationship("Item", backref="users")
   
 class Bid(Base):
-  __tablename__ = "users"
+  __tablename__ = "bids"
   
   id = Column(Integer, primary_key=True)
-  username = Column(String, nullable=False)
-  password = Column(String, nullable=False)
+  price = Column(String, nullable=False)
+  user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
+  item_id = Column(Integer, ForeignKey('items.id'),nullable=False)
+  
   
   
 Base.metadata.create_all(engine)
