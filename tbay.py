a2df2847a0174465fdb618e2d0b477d1ddb9afaf
John Moore <john@ncub8.com>
2015-02-09 16:16:08 +0000

    final code for tbay

--- tbay.py
+++ tbay.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Table, Column, Integer, String, DateTime
+from sqlalchemy import Table, Column, Integer, String, DateTime, ForeignKey, Float
 
 
 engine = create_engine('postgresql://action:action@localhost:5432/tbay')
@@ -23,7 +23,7 @@ class Item(Base):
   bids = relationship("Bid", backref="item")
   
 
-  
+ 
   
 class User(Base):
   __tablename__ = "users"
@@ -31,16 +31,41 @@ class User(Base):
   id = Column(Integer, primary_key=True)
   username = Column(String, nullable=False)
   password = Column(String, nullable=False)
-  items = relationship("Item", backref="users")
+  items = relationship("Item", backref="user")
+  bids = relationship("Bid",backref="user")
   
 class Bid(Base):
   __tablename__ = "bids"
   
   id = Column(Integer, primary_key=True)
-  price = Column(String, nullable=False)
+  price = Column(Float, nullable=False)
   user_id = Column(Integer, ForeignKey('users.id'),nullable=False)
   item_id = Column(Integer, ForeignKey('items.id'),nullable=False)
   
-  
-  
+
 Base.metadata.create_all(engine)
+
+bob = User(username="bob", password="bob1")
+joe = User(username="joe", password="joe1")
+sally = User(username="sally", password="sally1")
+
+baseball = Item(name="baseball",description="autographed baseball")
+
+bob.items.append(baseball)
+
+bid1 = Bid(price=22.95, user=sally, item=baseball)
+bid2 = Bid(price=21.97, user=joe, item=baseball)
+
+session.add_all([bob, joe, sally, baseball, bid1, bid2])
+session.commit()
+
+highest_bid = None
+
+for item in bob.items:
+  for bid in item.bids:
+    if highest_bid == None:
+      highest_bid = bid
+    elif bid.price > highest_bid.price:
+      highest_bid = bid
+  print "The highest bid on " + item.name + " is " + str(highest_bid.price) + " by " + highest_bid.user.username
+  
